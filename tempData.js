e6c2a41709b922a8c26184c292f2d8062e1303d1
Nomit Verma <nomitverma1@gmail.com>
2020-09-11 02:47:30 +0530

    Add temporary data for TodoList

---
+++ tempData.js
@@ -0,0 +1,24 @@
+const tempData = {
+    status : "success",
+    data : [
+        {
+            title: 'list1',
+            id: '1',
+        },
+        {
+            title: 'list2',
+            id: '2',
+        },
+        {
+            title: 'list3',
+            id: '3',
+        },
+        {
+            title: 'list4',
+            id: '4',
+        },
+    ]
+
+}
+
+export default tempData
