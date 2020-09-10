e7d410f1e38cfc9c8472d938620c133e70bc8e2b
Nomit Verma <nomitverma1@gmail.com>
2020-09-11 03:06:10 +0530

    Add Temporary Home Screen to navigate to on Login Implementation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import * as firebase from 'firebase'
+
+export class HomeScreen extends Component {
+    state = {
+        email : '',
+        displayName: ''
+    }
+
+    componentDidMount() {
+        const { email, displayName } = firebase.auth().currentUser
+        
+        this.setState({email, displayName})
+    }
+
+    signOutUser = () => {
+        firebase.auth().signOut()
+    }
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Hi {this.state.email}!</Text>
+            
+                <TouchableOpacity style={{ marginTop: 32}} onPress={this.signOutUser}>
+                    <Text>
+                        Logout
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+export default HomeScreen
