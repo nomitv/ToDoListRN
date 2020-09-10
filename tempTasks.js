3b43745a30b0ce4e4954f08a1dcb806d28b9836f
Nomit Verma <nomitverma1@gmail.com>
2020-09-11 02:48:17 +0530

    Add temporary data for Tasks

---
+++ tempTasks.js
@@ -0,0 +1,27 @@
+const tempTasks = {
+    status : "success",
+    data : [
+        {
+            title: 'task1',
+            id: '1',
+            listId: '2'
+        },
+        {
+            title: 'task2',
+            id: '2',
+            listId: '2'
+        },
+        {
+            title: 'task3',
+            id: '3',
+            listId: '2'
+        },
+        {
+            title: 'task4',
+            id: '4',
+            listId: '2'
+        },
+    ]
+}
+
+export default tempTasks
